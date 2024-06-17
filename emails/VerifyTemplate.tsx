9fa0f58264728dc59be956188a0e0beacec98574
0Sammy <156402795+0Sammy@users.noreply.github.com>
2024-06-17 08:58:34 +0100

    Update Logo

--- emails/VerifyTemplate.tsx
+++ emails/VerifyTemplate.tsx
@@ -16,7 +16,7 @@ export default function VerifyTemplate({ userName }: EmailProps) {
           <Container>
             <img
               style={image}
-              src="https://res.cloudinary.com/dpmx02shl/image/upload/v1711625589/keystoneNationalBank/logoOrange_krrbia.png"
+              src="https://res.cloudinary.com/dpmx02shl/image/upload/v1718604174/primepeak/logo_ssfys9.png"
               alt="Prime Peak Bank Logo"
             />
           </Container>
